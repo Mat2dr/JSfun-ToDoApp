7bd22270d3fa5a1d4b64b04bcaa3e2443f710502
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-09-25 17:08:30 +0200

    Clear imput text on click

--- scripts.js
+++ scripts.js
@@ -5,10 +5,10 @@ const listTasks = [];
 
 inputTask.addEventListener("input", (e) =>{
     task = e.target.value;
-    console.log(task)
 })
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
     listTasks.push(task);
+    console.log(listTasks);
 })
