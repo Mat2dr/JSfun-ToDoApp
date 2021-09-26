1b453695bcb345ba3035a7b4215d8a8bf747a65b
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-09-26 15:56:51 +0200

    Font add

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const inputTask = document.querySelector('input[type="text"]');
 const form = document.querySelector("form");
 let task = "";
 const listTasks = [];
+let numList = 0;
 
 inputTask.addEventListener("input", (e) =>{
     task = e.target.value;
@@ -10,9 +11,10 @@ inputTask.addEventListener("input", (e) =>{
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
     listTasks.push(task);
-    console.log(listTasks);
+    numList ++;
+    listTasks.slice('numList');
 
     document.querySelector("form > .tasks").innerHTML = `
-    <h4>${listTasks}</h4>
+    <h4>${listTasks.slice('numList')}</h4>
     `;
 })
