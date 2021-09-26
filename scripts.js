8b314b3ec7b8fbd7b3e97d5bebb396b63998656d
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-09-26 16:19:44 +0200

    Const querySelector

--- scripts.js
+++ scripts.js
@@ -1,20 +1,3 @@
-const inputTask = document.querySelector('input[type="text"]');
-const form = document.querySelector("form");
-let task = "";
-const listTasks = [];
-let numList = 0;
-
-inputTask.addEventListener("input", (e) =>{
-    task = e.target.value;
-})
-
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    listTasks.push(task);
-    numList ++;
-    listTasks.slice('numList');
-
-    document.querySelector("form > .tasks").innerHTML = `
-    <h4>${listTasks.slice('numList')}</h4>
-    `;
-})
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-input');
