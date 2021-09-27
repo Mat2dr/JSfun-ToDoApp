7715cefb2ce90793a1b2f1339b26627f4eb9ad83
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-09-27 14:36:56 +0200

    Ajout tache

--- scripts.js
+++ scripts.js
@@ -14,3 +14,19 @@ const options = {weekday: 'long', year: 'numeric', month: 'short', day: 'numeric
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("fr-FR", options);
+
+//Ajout tache function
+function addToDo(toDo){
+    const item = `<li class="item">
+                    <i class="fa fa-circle-thin co" job="completed" id="0"></i>
+                    <p class="text">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                </li>
+                `;
+    
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+addToDo("coder");
