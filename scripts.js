232f52c95d13844b677eb49b132641b86f3cabc5
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-09-27 12:04:11 +0200

    Index + submit element

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-input');
+//Selecteurs
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const item = document.getElementById('item');
+
+//Event
+//Ajout element
+form.addEventListener('submit', (e => {
+    list.innerHTML +=  `<li>${item.value}</li> `
+}))
