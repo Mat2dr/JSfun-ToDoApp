ae3b8a5b39a44fa3be3f59deaabcc8e9025fe051
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-09-27 14:16:26 +0200

    Main HTML et css

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
-//Selecteurs
-const list = document.getElementById('list');
-const form = document.getElementById('form');
-const item = document.getElementById('item');
+// Selecteurs
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById('item');
 
-//Event
-//Ajout element
-form.addEventListener('submit', (e => {
-    list.innerHTML +=  `<li>${item.value}</li> `
-}))
+// Event
+// Ajout element
+btn.addEventListener('onclick', (e) => {
+    e.preventDefault();
+    list.innerHTML += `<li>${item.value}</li>`;
+    item.value = '';
+});
